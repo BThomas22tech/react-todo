70292ffe6f48104e1b539b1e2fad366c2c9e37db
BThomas <bran_thom@yahoo.com>
2023-09-25 13:51:59 -0500

    created todolist and form components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
-const todoList = [{id:1,title:"complete assignment",},{id:2,title:"pick up kid"},{id:3,title:"sleep"}]
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
 function App() {
   return (
@@ -8,21 +8,10 @@ function App() {
       <h1>
         Todo List
       </h1>
-      <List />
+      <TodoList />
+      <AddTodoForm />
     </div>
   );
-}
-function List(){
-  return(
-    
-    <ul>
-        {todoList.map((item)=>{
-          return<li key={item.id}>
-            <span>{item.id} </span>
-            {item.title}</li>
-        })}
-      </ul>
-  )
 }
 
 export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const todoList = [{id:1,title:"complete assignment",},{id:2,title:"pick up kid"},{id:3,title:"sleep"}]
+
+function TodoList() {
+
+    return (
+
+        <ul>
+            {todoList.map((item) => {
+                return <li key={item.id}>
+                    {item.title}</li>
+            })}
+        </ul>
+    )
+}
+
+export default TodoList
