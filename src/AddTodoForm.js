f29e609e43e3ba8beca31a6e0d5505adbc32ba99
BThomas <bran_thom@yahoo.com>
2023-09-27 14:25:59 -0500

    update

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -2,11 +2,9 @@ import React from "react"
 
 function AddTodoForm(props){
     const handleChange = (event_item) =>{
-        props.onAddTodo(event_item.target.value)
         console.log(event_item.target.value)
-
-    } 
-    
+        props.onAddTodo(event_item.target.value)
+}    
 return (
     <form>
         <label htmlFor="todoTitle" id="todoTitle">
