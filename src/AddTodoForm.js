d1044a46d308d13a8e386d9fce11cd26ab4ce5aa
BThomas <bran_thom@yahoo.com>
2023-10-01 14:59:05 -0500

    formatted document

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,28 +1,33 @@
-import React from "react"
+import React from "react";
 
-function AddTodoForm(props){
-    const handleChange = (event_item) =>{
+function AddTodoForm(props) {
+    const handleChange = (event_item) => {
         // console.log(event_item.target.value)
-        props.onAddTodo(event_item.target.value)
-}    
+        props.onAddTodo(event_item.target.value);
+    };
 
-function handleAddTodo(events){
-    events.preventDefault()
-    const todoTitle = events.target.elements.title.value
-    console.log(todoTitle)
-    props.onAddTodo(todoTitle)
-    events.target.reset()
+    function handleAddTodo(events) {
+        events.preventDefault();
+        const todoTitle = events.target.elements.title.value;
+        console.log(todoTitle);
+        props.onAddTodo(todoTitle);
+        events.target.reset();
+    }
+
+    return (
+        <form onSubmit={handleAddTodo}>
+            <label htmlFor="todoTitle" id="todoTitle">
+                Title
+            </label>
+            <input
+                type="text"
+                id="todoTitle"
+                name="title"
+                onChange={handleChange}
+            ></input>
+            <button type="submit">Add</button>
+        </form>
+    );
 }
 
-return (
-    <form onSubmit={handleAddTodo}>
-        <label htmlFor="todoTitle" id="todoTitle">
-            Title
-        </label>
-        <input type="text" id="todoTitle" name="title"onChange={handleChange}></input>
-        <button type="submit" >Add</button>
-    </form>
-)
-}
-
-export default AddTodoForm
+export default AddTodoForm;
--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import TodoList from './TodoList';
-import AddTodoForm from './AddTodoForm';
-
+import React from "react";
+import TodoList from "./TodoList";
+import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  const todoList = [{id:1,title:"complete assignment",},{id:2,title:"pick up kid"},{id:3,title:"sleep"}]
-  const [newTodo,setNewTodo] = React.useState('')
+  const todoList = [
+    { id: 1, title: "complete assignment" },
+    { id: 2, title: "pick up kid" },
+    { id: 3, title: "sleep" }
+  ];
+  const [newTodo, setNewTodo] = React.useState("");
 
   return (
     <div>
-      <h1>
-        Todo List
-      </h1>
+      <h1>Todo List</h1>
       <AddTodoForm onAddTodo={setNewTodo} />
-      <p>New Item: <strong>{newTodo}</strong></p>
+      <p>
+        New Item: <strong>{newTodo}</strong>
+      </p>
       <hr />
-      <TodoList todoList={todoList}/>
-      
+      <TodoList todoList={todoList} />
     </div>
   );
 }
