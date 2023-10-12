95e72fc9dca08971a3cb280f2fbf57c4448849fa
BThomas <bran_thom@yahoo.com>
2023-10-12 09:08:22 -0500

    move todo llist into state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  const todoList = [
+  const todo= [
     { id: 1, title: "complete assignment" },
     { id: 2, title: "pick up kid" },
     { id: 3, title: "sleep" }
   ];
   const [newTodo, setNewTodo] = React.useState("");
-
+  const [todoList,setTodoList] = React.useState([])
   return (
     <div>
       <h1>Todo List</h1>
