03ea112005eb87dc0921e98204ff8adf055b3d34
BThomas <bran_thom@yahoo.com>
2023-10-24 16:39:55 -0500

    todo list saved when with refreshed browser

--- src/App.js
+++ src/App.js
@@ -3,24 +3,22 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
-
-  const [todoList,setTodoList] = React.useState([])
-
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')) || React);
   
-  React.useEffect(() =>{
-    localStorage.setItem('savedTodoList',todoList);
-  },[todoList])
+  React.useEffect(() => {
+    const todoListString = JSON.stringify(todoList);
+    localStorage.setItem("savedTodoList", todoListString);
+  }, [todoList]);
 
-
-  function addTodo(newTodo){
-    setTodoList([...todoList,newTodo])
+  function addTodo(newTodo) {
+    setTodoList([...todoList, newTodo]);
   }
 
   return (
     <div>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      
+
       <hr />
       <TodoList todoList={todoList} />
     </div>
