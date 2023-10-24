fdcdb493e59f857e5f049f03a1ca9a9d65cacd8e
BThomas <bran_thom@yahoo.com>
2023-10-24 17:23:26 -0500

    formatted document

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
-function useSemiPersistentState(){
-  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')) || React);
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = React.useState(
+    JSON.parse(localStorage.getItem("savedTodoList")) || React
+  );
   React.useEffect(() => {
     const todoListString = JSON.stringify(todoList);
     localStorage.setItem("savedTodoList", todoListString);
   }, [todoList]);
 
-  return [todoList,setTodoList] 
+  return [todoList, setTodoList];
 }
 function App() {
   const [todoList, setTodoList] = useSemiPersistentState();
