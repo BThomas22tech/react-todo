52c116040c5be6113e4038b2e0e7606bd3a8e783
BThomas <bran_thom@yahoo.com>
2023-11-12 11:58:18 -0600

    formatting

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,23 +1,23 @@
 import React, { useRef, useEffect } from "react";
 
 function InputWithLabel(props) {
-  const inputRef = useRef();
-  useEffect(() => {
-    inputRef.current.focus();
-  }, []);
-  return (
-    <>
-      <label label={props.title}>{props.children}</label>
-      <input
-        type="text"
-        id="todoTitle"
-        name="title"
-        value={props.todoTitle}
-        onChange={props.handleTitleChange}
-        ref={inputRef}
-      ></input>
-    </>
-  );
+    const inputRef = useRef();
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
+    return (
+        <>
+            <label label={props.title}>{props.children}</label>
+            <input
+                type="text"
+                id="todoTitle"
+                name="title"
+                value={props.todoTitle}
+                onChange={props.handleTitleChange}
+                ref={inputRef}
+            ></input>
+        </>
+    );
 }
 
 export default InputWithLabel;
