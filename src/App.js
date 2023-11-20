baef3c0623282be6d595d0b5cdbb996b2133986b
BThomas <bran_thom@yahoo.com>
2023-11-20 00:04:07 -0600

    add loading state and loading

--- src/App.js
+++ src/App.js
@@ -3,29 +3,30 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
-  // const initialList = JSON.parse(localStorage.getItem("savedTodoList")) || [];
-  // console.log(initialList);
+  const [isLoading, setIsLoading] = React.useState(true);
+
   const [todoList, setTodoList] = React.useState([]);
-
-  React.useEffect(() => {
-    const todoListString = JSON.stringify(todoList);
-    localStorage.setItem("savedTodoList", todoListString);
-  }, [todoList]);
-
   React.useEffect(() => {
     new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve({ data: { todoList: todoList} });
+        resolve({
+          data: {
+            todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
+          },
+        });
       }, 2000);
     }).then((result) => {
+      setIsLoading(false);
       setTodoList(result.data.todoList);
     });
   }, []);
 
   React.useEffect(() => {
-    const todoListString = JSON.stringify(todoList);
-    localStorage.setItem("savedTodoList", todoListString);
-  }, [todoList]);
+    if (isLoading === false) {
+      const todoListString = JSON.stringify(todoList);
+      localStorage.setItem("savedTodoList", todoListString);
+    }
+  }, [isLoading, todoList]);
 
   function handleremoveTodo(id) {
     const removeTodo = todoList.filter((todo) => id !== todo.id);
