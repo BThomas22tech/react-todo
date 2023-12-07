02d2a80d65cfb3ad0f6b712d6490c7512d8685ae
BThomas <bran_thom@yahoo.com>
2023-12-07 17:59:49 -0600

    async set up

--- src/App.js
+++ src/App.js
@@ -7,20 +7,44 @@ function App() {
 
   const [todoList, setTodoList] = React.useState([]);
 
+  async function fetchData() {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_AIRTABLE_API_TOKEN}`,
+      },
+    };
+    // const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}/`;
+    console.log((process.env.REACT_APP_AIRTABLE_BASE_ID))
+    // try {
+    //   const response = await fetch(url, options);
+    //   if (!response.ok) {
+    //     const message = `Error has ocurred:${response.status}`;
+    //     // throw new Error(message);
+    //     console.log(message);
+    //   }
+    //   const data = await response.json();
+    //   console.log(data);
+    // } catch (error) {
+    //   console.error(error.message);
+    // }
+  }
+
   React.useEffect(() => {
-    new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          data: {
-            todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
-          },
-        });
-      }, 2000);
-    }).then((result) => {
-      setTodoList(result.data.todoList);
-      setIsLoading(false);
-    });
-  }, []);
+    fetchData()
+    // new Promise((resolve, reject) => {
+    //   setTimeout(() => {
+    //     resolve({
+    //       data: {
+    //         todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
+    //       },
+    //     });
+    //   }, 2000);
+    // }).then((result) => {
+    //   setTodoList(result.data.todoList);
+    //   setIsLoading(false);
+    // });
+  });
 
   React.useEffect(() => {
     if (isLoading === false) {
@@ -41,13 +65,15 @@ function App() {
 
   return (
     <>
-    <h1>Todo List</h1>
+      <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
-      
 
       <hr />
-      {isLoading ? "Loading..." :
-      <TodoList todoList={todoList} onRemoveTodo={handleremoveTodo} />}
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <TodoList todoList={todoList} onRemoveTodo={handleremoveTodo} />
+      )}
     </>
   );
 }
